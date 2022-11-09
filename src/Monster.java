93718db9acaba82bdb1e79efec7a3dd85cbbfa7e
TOMMASO.NONES <TOMMASO.NONES@buonarroti.tn.it>
2022-11-09 16:09:40 +0100

    feat:Creazione della classe Monster

---
+++ src/Monster.java
@@ -0,0 +1,27 @@
+public class Monster {
+    private String nome;
+    private float HP;
+    private float danno;
+
+    public Monster(String nome, float HP) {
+        this.nome = nome;
+        this.HP = HP;
+    }
+
+    float damage(){
+
+    }
+
+    void subisci(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "nome='" + nome + '\'' +
+                ", HP=" + HP +
+                ", danno=" + danno +
+                '}';
+    }
+}
